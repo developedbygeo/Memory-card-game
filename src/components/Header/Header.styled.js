b3a9671112c1577d623af9154bf58ff1fb903b61
georgiosan <georgiosanagnostou@hotmail.com>
2022-02-15 01:48:32 +0200

    feat: adding the StyledHeader component

---
+++ src/components/Header/Header.styled.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import { flexMixin } from '../shared/mixins';
+
+const StyledHeader = styled.header`
+  background: ${({ theme }) => theme.colors.headerBg};
+  & > div {
+    user-select: none;
+    width: 90%;
+    height: 100%;
+    margin: auto;
+    color: ${({ theme }) => theme.colors.white};
+    ${flexMixin('flex-start', 'center', 'row')};
+    & > img {
+      height: 100%;
+    }
+    & > h1 {
+      margin-left: 1rem;
+    }
+  }
+`;
+
+export default StyledHeader;
