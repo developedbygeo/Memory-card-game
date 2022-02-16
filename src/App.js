b69af0b14ffd1dcca650192a480e1bb8565e0228
georgiosan <georgiosanagnostou@hotmail.com>
2022-02-16 03:22:44 +0200

    refactor: app now dispatches the custom action creator on mount

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getDogData } from './store/dogData-actions';
 import { ThemeProvider } from 'styled-components/macro';
-import themeSettings from './components/shared/themeSettings';
 import GlobalStyle from './components/shared/globalStyle';
+import themeSettings from './components/shared/themeSettings';
 import Header from './components/Header/Header';
+import ScoreBoard from './components/Score/ScoreBoard';
+import CardBoard from './components/Cards/CardBoard';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getDogData());
+  }, [dispatch]);
+
   return (
     <ThemeProvider theme={themeSettings}>
       <GlobalStyle />
       <Header />
-      <main></main>
+      <main>
+        <ScoreBoard />
+        <CardBoard />
+      </main>
     </ThemeProvider>
   );
 }
