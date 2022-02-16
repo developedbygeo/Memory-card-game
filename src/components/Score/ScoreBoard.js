b3f0d33a3ee11bbdcb2c4b61e35b3df8cfa017c2
georgiosan <georgiosanagnostou@hotmail.com>
2022-02-16 03:26:59 +0200

    feat: adding the score board for both the current & best scores

---
+++ src/components/Score/ScoreBoard.js
@@ -0,0 +1,19 @@
+import { StyledSection } from '../shared/UI/Container.styled';
+import Score from './Score';
+
+const sectionFlexSettings = {
+  align: 'center',
+  justify: 'space-evenly',
+  dir: 'row',
+};
+
+const ScoreBoard = () => {
+  return (
+    <StyledSection flexSettings={sectionFlexSettings} width="80%">
+      <Score type="Current" />
+      <Score type="Best" />
+    </StyledSection>
+  );
+};
+
+export default ScoreBoard;
