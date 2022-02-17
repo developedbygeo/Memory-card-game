c6784523d86a2f994906e8b90aef91d8b8265ad7
georgiosan <georgiosanagnostou@hotmail.com>
2022-02-17 22:26:30 +0200

    feat: app will now render the intro modal on load and controls the theme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import { useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 import { getDogData } from './store/dogData-actions';
-import { ThemeProvider } from 'styled-components/macro';
+import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './components/shared/globalStyle';
-import themeSettings from './components/shared/themeSettings';
+import { lightTheme, darkTheme } from './components/shared/themeSettings';
+import Modal from './components/shared/UI/Modal/Modal';
+import { IntroDialogue } from './components/shared/UI/Modal/Dialogues';
 import Header from './components/Header/Header';
 import ScoreBoard from './components/Score/ScoreBoard';
 import CardBoard from './components/Cards/CardBoard';
 
 function App() {
+  const [showIntro, setShowIntro] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
   const dispatch = useDispatch();
+
+  const themeChangeHandler = useCallback(() => setIsDarkTheme((prevState) => !prevState), []);
+
+  useEffect(() => {
+    setShowIntro(true);
+  }, []);
 
   useEffect(() => {
     dispatch(getDogData());
   }, [dispatch]);
 
+  const disableModalHandler = useCallback(() => setShowIntro((prevState) => !prevState), []);
+
+  const introModal = (
+    <Modal onDisable={disableModalHandler} purpose="intro">
+      <IntroDialogue onDisable={disableModalHandler} />
+    </Modal>
+  );
+
   return (
-    <ThemeProvider theme={themeSettings}>
+    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
       <GlobalStyle />
-      <Header />
+      <Header onThemeChange={themeChangeHandler} darkThemeEnabled={isDarkTheme} />
+      {showIntro && introModal}
       <main>
         <ScoreBoard />
         <CardBoard />
