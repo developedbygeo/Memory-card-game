5f3b5adcc63d01d9ea5a531a47e280687760df08
georgiosan <georgiosanagnostou@hotmail.com>
2022-02-17 22:24:05 +0200

    feat: introducing the theme-changing button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,25 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun as LightIcon, faMoon as DarkIcon } from '@fortawesome/free-solid-svg-icons';
 import StyledHeader from './Header.styled';
+import { ThemeButton } from '../shared/UI/Button.styled';
 import dogImage from '../../assets/shiba-dog.png';
 
-const Header = () => {
+const Header = ({ onThemeChange, darkThemeEnabled }) => {
+  const futureTheme = darkThemeEnabled ? 'light' : 'dark';
+  const themeIcon = darkThemeEnabled ? DarkIcon : LightIcon;
+
   return (
     <StyledHeader>
       <div>
         <img src={dogImage} alt="colorful-sketch-of-a-dog" />
         <h1>Memory Card Game</h1>
       </div>
+      <ThemeButton onClick={onThemeChange} title={`Change to ${futureTheme} theme`}>
+        <FontAwesomeIcon icon={themeIcon} />
+      </ThemeButton>
     </StyledHeader>
   );
 };
 
-export default Header;
+export default React.memo(Header);
