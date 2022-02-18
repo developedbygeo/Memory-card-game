89ca37f36f82ed74a67b09a72f244c0ef4e812d4
georgiosan <georgiosanagnostou@hotmail.com>
2022-02-19 01:40:35 +0200

    feat: rendering the fetched data into clickable cards

--- src/components/Cards/CardBoard.js
+++ src/components/Cards/CardBoard.js
@@ -1,4 +1,7 @@
+import { useSelector } from 'react-redux';
+
 import { StyledSection } from '../shared/UI/Container.styled';
+import { LoadingSpinner } from '../shared/UI/LoadingSpinner';
 import MemoryCard from './MemoryCard';
 
 const sectionGridSettings = {
@@ -7,7 +10,21 @@ const sectionGridSettings = {
 };
 
 const CardBoard = () => {
-  return <StyledSection width="90%" gridSettings={sectionGridSettings}></StyledSection>;
+  const { fetched, data } = useSelector((state) => state.dogData);
+
+  // TODO use fetched to either render dogCards or the loading spinner & err msg
+
+  const dogCards = data.map((element) => (
+    <MemoryCard key={element.key} imageCont="true">
+      <img src={element.path} alt={`a lovely dog fetched online - ${element.key}/${data.length}`} />
+    </MemoryCard>
+  ));
+
+  return (
+    <StyledSection width="70%" customGap="1rem" gridSettings={sectionGridSettings}>
+      {dogCards}
+    </StyledSection>
+  );
 };
 
 export default CardBoard;
