392677a71a3ec7fff6906da814864e80174922b6
georgiosan <georgiosanagnostou@hotmail.com>
2022-02-18 20:58:51 +0200

    refactor: passing the individual props

--- src/components/shared/UI/Notification/Notification.js
+++ src/components/shared/UI/Notification/Notification.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import StyledNotification from './Notification.styled';
 
-const Notification = ({ data }) => {
-  const { status, title, message } = data;
+const Notification = ({ status, title, message }) => {
   return (
     <StyledNotification status={status}>
       <h2>{title}</h2>
