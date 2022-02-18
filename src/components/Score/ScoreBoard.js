7da3c759209163bac27224b8dfc92ba2c0858ab3
georgiosan <georgiosanagnostou@hotmail.com>
2022-02-19 01:37:53 +0200

    style: adjusting the layout setings and score  background clr

--- src/components/Score/ScoreBoard.js
+++ src/components/Score/ScoreBoard.js
@@ -3,15 +3,15 @@ import Score from './Score';
 
 const sectionFlexSettings = {
   align: 'center',
-  justify: 'space-evenly',
+  justify: 'space-between',
   dir: 'row',
 };
 
 const ScoreBoard = () => {
   return (
-    <StyledSection flexSettings={sectionFlexSettings} width="80%">
-      <Score type="Current" />
-      <Score BgClr={'#ef4444'} type="Best" />
+    <StyledSection flexSettings={sectionFlexSettings} width="70%">
+      <Score BgClr="#6482a6" type="Current" />
+      <Score BgClr="#ef4444" type="Best" />
     </StyledSection>
   );
 };
