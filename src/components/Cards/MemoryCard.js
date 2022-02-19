a6c6ac2d43f296adb342afcb7bf459f3e86c6b45
georgiosan <georgiosanagnostou@hotmail.com>
2022-02-19 22:13:47 +0200

    chore: passing down the onDogSelection prop

--- src/components/Cards/MemoryCard.js
+++ src/components/Cards/MemoryCard.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Card from '../shared/UI/Card.styled';
 
-const MemoryCard = ({ children, imageCont }) => {
-  return <Card imageCont={imageCont}>{children}</Card>;
+const MemoryCard = ({ children, imageCont, onDogSelection }) => {
+  return (
+    <Card onClick={onDogSelection} imageCont={imageCont}>
+      {children}
+    </Card>
+  );
 };
 
 export default React.memo(MemoryCard);
