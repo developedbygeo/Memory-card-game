f264c9aca8c10ca3501aa3d65b2a28abb6c5497d
georgiosan <georgiosanagnostou@hotmail.com>
2022-02-21 00:43:02 +0200

    refactor: making app leaner with the addition of the second custom hook

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
 import useInitial from './hooks/useInitial';
+import useWinner from './hooks/useWinner';
 
 import { getDogData } from './store/dogData-actions';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './components/shared/globalStyle';
 import { lightTheme, darkTheme } from './components/shared/themeSettings';
-import Modal from './components/shared/UI/Modal/Modal';
-import { IntroDialogue } from './components/shared/UI/Modal/Dialogues';
 import Header from './components/Header/Header';
 import Notification from './components/shared/UI/Notification/Notification';
 import ScoreBoard from './components/Score/ScoreBoard';
 import CardBoard from './components/Cards/CardBoard';
 
 function App() {
-  const { theme, intro, disableModalHandler, shouldNotificationRender, notificationData } =
-    useInitial(getDogData);
-
-  const introModal = (
-    <Modal onDisable={disableModalHandler} purpose="intro">
-      <IntroDialogue onDisable={disableModalHandler} />
-    </Modal>
-  );
+  const { theme, intro, shouldNotificationRender, notificationData, introModal } = useInitial(getDogData);
+  const { showWinnerDialogue, winnerModal } = useWinner();
 
   return (
     <ThemeProvider theme={theme.status ? darkTheme : lightTheme}>
       <GlobalStyle />
       <Header onThemeChange={theme.handler} darkThemeEnabled={theme.status} />
       {intro.status && introModal}
+      {showWinnerDialogue && winnerModal}
       <main>
         <ScoreBoard />
         <CardBoard />
