9354f59d98f3df21c68e991dcb2fa25412476672
georgiosan <georgiosanagnostou@hotmail.com>
2022-02-20 03:29:15 +0200

    feat: adding dynamic text animation based on score change

--- src/components/Score/Score.js
+++ src/components/Score/Score.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Card from '../shared/UI/Card.styled';
 
 const Score = ({ type, score, BgClr }) => {
+  const [animationActive, setAnimationActive] = useState(false);
+
+  useEffect(() => {
+    setAnimationActive(true);
+    const timer = setTimeout(() => {
+      setAnimationActive(false);
+    }, 300);
+    return () => clearTimeout(timer);
+  }, [score]);
+
+  const animationClass = animationActive ? 'bump' : '';
+
   return (
     <Card BgClr={BgClr}>
-      <h2>
+      <h2 className={animationClass}>
         {type} Score: <span>{score}</span>
       </h2>
     </Card>
